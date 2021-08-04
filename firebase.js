c6dcd47dd1349985d27f70ae069fc46e1e5db840
zill4 <justcrisp@outlook.com>
2021-08-04 03:51:02 -0700

    updated routes

--- firebase.js
+++ firebase.js
@@ -1,22 +1,28 @@
-// import firebase from "firebase/app"
-// import "firebase/auth"
-// import "firebase/database";
-// import "firebase/firestore";
-// import "firebase/storage";
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/database";
+import "firebase/firestore";
+import "firebase/storage";
 
-// const app = firebase.initializeApp({
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-// })
 
-// export const auth = app.auth()
-// export const database = firebase.database();
-// export const firestore = firebase.firestore();
-// export const storage = firebase.storage();
-// export default app
+const FirebaseCredentials = {
+  apiKey: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_MEASUREMENT_ID
+}
+
+if (!firebase.apps.length)
+{
+    firebase.initializeApp(FirebaseCredentials)
+}
+
+export const auth = firebase.auth()
+export const database = firebase.database();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
+export default firebase;    
